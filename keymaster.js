5da94f607e91e8fe4c42e5afd6667a73ecbffb2d
Christoph M. Becker <cmbecker69@gmx.de>
2022-08-21 17:23:11 +0200

    Flatten JS "architecture"
    There is no need for a class; a couple of variables and functions are
    fully sufficient (besides that JS classes are … JS classes).

--- keymaster.js
+++ keymaster.js
@@ -8,209 +8,162 @@
  * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3
  */
 
+(function () {
 /**
- * The Keymaster class.
+ * The warning dialog.
  *
- * @constructor
- *
+ * @type {HTMLDivElement}
  */
-Keymaster = function () {
-    /**
-     * The warning dialog.
-     *
-     * @type {HTMLDivElement}
-     *
-     * @private
-     */
-    this._warningDialog = null;
+var warningDialog = null;
 
-    /**
-     * The warning text.
-     *
-     * @type {Text}
-     *
-     * @private
-     */
-    this._warningText = '';
+/**
+ * The warning text.
+ *
+ * @type {Text}
+ */
+var warningText = '';
 
-    /**
-     * The button text.
-     *
-     * @type {Text}
-     *
-     * @private
-     */
-    this._buttonText = '';
+/**
+ * The button text.
+ *
+ * @type {Text}
+ */
+var buttonText = '';
 
-    /**
-     * The timer handle.
-     *
-     * @type {Number}
-     *
-     * @private
-     */
-    this._timer = null;
-
-    this.initWarningDialog();
-    this._timer = setInterval(this.requestRemainingTime.bind(this),
-            Keymaster.config.pollInterval);
-};
+/**
+ * The timer handle.
+ *
+ * @type {Number}
+ */
+var timer = null;
 
 /**
  * The configuration.
  *
  * @type {Object}
- *
- * @static
  */
-Keymaster.config = JSON.parse(document.getElementsByName("keymaster_config")[0].content);
+var config = JSON.parse(document.getElementsByName("keymaster_config")[0].content);
 
 /**
  * The localization
  *
  * @type {Object}
- *
- * @static
  */
-Keymaster.l10n = JSON.parse(document.getElementsByName("keymaster_lang")[0].content);
+var l10n = JSON.parse(document.getElementsByName("keymaster_lang")[0].content);
 
 /**
  * Initializes the warning dialog.
- *
- * @method
- *
- * @protected
  */
-Keymaster.prototype.initWarningDialog = function () {
+function initWarningDialog() {
     var button;
 
-    this._warningDialog = document.getElementById("keymaster");
-    this._warningText = this._warningDialog.getElementsByTagName("p")[0].firstChild;
-    button = this._warningDialog.getElementsByTagName("button")[0];
-    button.onclick = this.resetSession.bind(this);
-    this._buttonText = button.firstChild;
-};
+    warningDialog = document.getElementById("keymaster");
+    warningText = warningDialog.getElementsByTagName("p")[0].firstChild;
+    button = warningDialog.getElementsByTagName("button")[0];
+    button.onclick = resetSession;
+    buttonText = button.firstChild;
+}
 
 /**
  * Shows the warning dialog.
- *
- * @method
- *
- * @protected
  */
-Keymaster.prototype.showWarningDialog = function () {
-    this._warningDialog.style.display = "block";
-};
+function showWarningDialog() {
+    warningDialog.style.display = "block";
+}
 
 /**
  * Hides the warning dialog.
- *
- * @method
- *
- * @protected
  */
-Keymaster.prototype.hideWarningDialog = function () {
-    this._warningDialog.style.display = "none";
-};
+function hideWarningDialog() {
+    warningDialog.style.display = "none";
+}
 
 /**
  * Requests the remaining session time from the server.
- *
- * @method
- *
- * @protected
  */
-Keymaster.prototype.requestRemainingTime = function () {
+function requestRemainingTime() {
     var request = new XMLHttpRequest();
     var url = "./?&keymaster_time";
 
     request.open("GET", url);
-    request.onreadystatechange = this.receiveRemainingTime.bind(this, request);
+    request.onreadystatechange = function () {
+        receiveRemainingTime(request);
+    }
     request.send(null);
-};
+}
 
 /**
  * Handles responses for the requested session time.
  *
- * @method
- *
  * @param {XMLHttpRequest} request A request object.
- *
- * @protected
  */
-Keymaster.prototype.receiveRemainingTime = function (request) {
+function receiveRemainingTime(request) {
     if (request.readyState == 4 && request.status == 200) {
         var remaining = +request.responseText;
         if (remaining != remaining) {
-            clearInterval(this._timer);
-            if (confirm(Keymaster.l10n.error)) {
+            clearInterval(timer);
+            if (confirm(l10n.error)) {
                 open(url);
             }
         } else if (remaining < 0) {
-            clearInterval(this._timer);
-            this.updateWarning(remaining);
+            clearInterval(timer);
+            updateWarning(remaining);
             location.href = location.href;
         } else if (remaining == 0) {
-            clearInterval(this._timer);
-            this.updateWarning(remaining);
+            clearInterval(timer);
+            updateWarning(remaining);
             location.href = "./?&logout";
-        } else if (remaining < Keymaster.config.warn) {
-            this.updateWarning(remaining);
-            this.showWarningDialog();
+        } else if (remaining < config.warn) {
+            updateWarning(remaining);
+            showWarningDialog();
         } else {
-            this.hideWarningDialog();
+            hideWarningDialog();
         }
     }
-};
+}
 
 /**
  * Resets the session timeout.
- *
- * @method
- *
- * @protected
  */
-Keymaster.prototype.resetSession = function () {
+function resetSession() {
     var request = new XMLHttpRequest();
 
     request.open("POST", "./");
     request.setRequestHeader("Content-Type",
             "application/x-www-form-urlencoded");
     request.send("keymaster_reset");
-    this.hideWarningDialog();
-};
+    hideWarningDialog();
+}
 
 /**
  * Updates the warning message.
  *
- * @method
- *
  * @param {Number} seconds The remaining time in seconds.
- *
- * @protected
  */
-Keymaster.prototype.updateWarning = function (seconds) {
+function updateWarning(seconds) {
     var min;
 
     if (seconds > 0) {
         min = Math.ceil(seconds / 60);
         if (min == 1) {
-            text = Keymaster.l10n.warning_singular;
+            text = l10n.warning_singular;
         } else if (min >= 2 && min <= 4) {
-            text = Keymaster.l10n.warning_paucal;
+            text = l10n.warning_paucal;
         } else {
-            text = Keymaster.l10n.warning_plural;
+            text = l10n.warning_plural;
         }
-        this._warningText.nodeValue = text.replace(/{{{TIME}}}/, min);
+        warningText.nodeValue = text.replace(/{{{TIME}}}/, min);
     } else {
-        this._warningText.nodeValue = Keymaster.l10n.warning_loggedout;
-        this._buttonText.nodeValue = Keymaster.l10n.button_ok;
+        warningText.nodeValue = l10n.warning_loggedout;
+        buttonText.nodeValue = l10n.button_ok;
     }
-};
+}
 
 /*
  * Register Keymaster instantiation on load.
  */
 addEventListener("load", function () {
-    new Keymaster();
+    initWarningDialog();
+    timer = setInterval(requestRemainingTime, config.pollInterval);
 });
+}());
