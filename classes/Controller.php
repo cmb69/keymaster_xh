7edb1093f4d126f967e423533cda5e19b55b757e
Christoph M. Becker <cmbecker69@gmx.de>
2022-09-23 15:03:24 +0200

    Switch to GNU style file headers

--- classes/Controller.php
+++ classes/Controller.php
@@ -1,16 +1,22 @@
 <?php
 
 /**
- * The controller class.
+ * Copyright (C) 2013-2019 Christoph M. Becker
  *
- * PHP versions 4 and 5
+ * This file is part of Keymaster_XH.
  *
- * @category  CMSimple_XH
- * @package   Keymaster
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2013-2019 Christoph M. Becker <http://3-magi.net/>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Keymaster_XH
+ * Keymaster_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Keymaster_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Keymaster_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace Keymaster;
